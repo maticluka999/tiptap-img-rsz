639b701d23b976070d91fe5cd2cfeda961106f9b
Luka Matic <101013503+maticluka999@users.noreply.github.com>
2023-08-30 21:13:21 +0200

    removed default width and height

--- src/image.tsx
+++ src/image.tsx
@@ -1,70 +1,75 @@
-import React, {Component, FC, ReactElement} from "react";
-import {mergeAttributes, nodeInputRule, Node} from "@tiptap/core";
-import {ReactNodeViewRenderer} from "@tiptap/react";
-import ImageResizeComponent from "./component/ImageResizeComponent";
-import Image from '@tiptap/extension-image'
+import React, { Component, FC, ReactElement } from 'react';
+import { mergeAttributes, nodeInputRule, Node } from '@tiptap/core';
+import { ReactNodeViewRenderer } from '@tiptap/react';
+import ImageResizeComponent from './component/ImageResizeComponent';
+import Image from '@tiptap/extension-image';
 
 export interface ImageOptions {
-  inline: boolean,
-  allowBase64: boolean,
-  HTMLAttributes: Record<string, any>,
-  resizeIcon: FC|Component|ReactElement,
-  useFigure: boolean
+  inline: boolean;
+  allowBase64: boolean;
+  HTMLAttributes: Record<string, any>;
+  resizeIcon: FC | Component | ReactElement;
+  useFigure: boolean;
 }
 declare module '@tiptap/core' {
   interface Commands<ReturnType> {
     imageResize: {
-      setImage: (options: { src: string, alt?: string, title?: string, width?: string|number, height?: string|number, isDraggable?: boolean }) => ReturnType,
-    }
+      setImage: (options: {
+        src: string;
+        alt?: string;
+        title?: string;
+        width?: string | number;
+        height?: string | number;
+        isDraggable?: boolean;
+      }) => ReturnType;
+    };
   }
 }
-export const inputRegex = /(!\[(.+|:?)]\((\S+)(?:(?:\s+)["'](\S+)["'])?\))$/
+export const inputRegex = /(!\[(.+|:?)]\((\S+)(?:(?:\s+)["'](\S+)["'])?\))$/;
 export const ImageResize = Image.extend<ImageOptions>({
-  name: "imageResize",
+  name: 'imageResize',
   addOptions() {
     return {
       inline: false,
       allowBase64: false,
       HTMLAttributes: {},
       resizeIcon: <>⊙</>,
-      useFigure: false
-    }
+      useFigure: false,
+    };
   },
   addAttributes() {
     return {
       ...this.parent?.(),
       width: {
-        default: '100%',
         renderHTML: (attributes) => {
           return {
-            width: attributes.width
+            width: attributes.width,
           };
-        }
+        },
       },
       height: {
-        default: 'auto',
         renderHTML: (attributes) => {
           return {
-            height: attributes.height
+            height: attributes.height,
           };
-        }
+        },
       },
     };
   },
 
   addNodeView() {
-    return ReactNodeViewRenderer(ImageResizeComponent)
+    return ReactNodeViewRenderer(ImageResizeComponent);
   },
   addInputRules() {
     return [
       nodeInputRule({
         find: inputRegex,
         type: this.type,
-        getAttributes: match => {
-          const [,, alt, src, title, height, width, isDraggable] = match
-          return { src, alt, title, height, width, isDraggable }
+        getAttributes: (match) => {
+          const [, , alt, src, title, height, width, isDraggable] = match;
+          return { src, alt, title, height, width, isDraggable };
         },
       }),
-    ]
+    ];
   },
-})
+});
